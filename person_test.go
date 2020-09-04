f6187982c1ada6717f07865df0050773274f6fc6
Fernando Almeida <dealmeida.fernando96@yahoo.com.br>
2020-09-04 14:22:47 -0300

    [label] [TB] test: edit test

--- person_test.go
+++ person_test.go
@@ -11,7 +11,7 @@ func TestGetPeople(t *testing.T) {
 		w    http.ResponseWriter
 		r    *http.Request
 	}{
-		// test test aaaaabbbbadfdfsafssssdsfdsfasdf
+		// test test aaaa
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
